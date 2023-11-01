d8e01bc102e76014ceb8446ea461f3f965aebf5f
EbenezerIyaju <ebenezeriyaju@gmail.com>
2023-11-01 20:42:20 +0100

    Added publish settings and exception handling

--- WinUpdateDelivery/DeliveryOptmizationHandler.cs
+++ WinUpdateDelivery/DeliveryOptmizationHandler.cs
@@ -12,15 +12,37 @@ namespace WinUpdateDelivery
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            try
             {
-                // Stop Windows Delivery Optimization service
-                StopService("DoSvc");
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    // Stop Windows Delivery Optimization service
+                    StopService("DoSvc");
 
-                // Stop Windows Update service
-                StopService("wuauserv");
+                    // Stop Windows Update service
+                    StopService("wuauserv");
 
-                await Task.Delay(5000, stoppingToken); // Check every 5 seconds
+                    await Task.Delay(5000, stoppingToken); // Check every 5 seconds
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // When the stopping token is canceled, for example, a call made from services.msc,
+                // we shouldn't exit with a non-zero exit code. In other words, this is expected...
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "{Message}", ex.Message);
+
+                // Terminates this process and returns an exit code to the operating system.
+                // This is required to avoid the 'BackgroundServiceExceptionBehavior', which
+                // performs one of two scenarios:
+                // 1. When set to "Ignore": will do nothing at all, errors cause zombie services.
+                // 2. When set to "StopHost": will cleanly stop the host, and log errors.
+                //
+                // In order for the Windows Service Management system to leverage configured
+                // recovery options, we need to terminate the process with a non-zero exit code.
+                Environment.Exit(1);
             }
         }
 
@@ -32,7 +54,7 @@ namespace WinUpdateDelivery
                 {
                     service.Stop();
                     service.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromSeconds(5));
-                    _logger.LogInformation($"{serviceName} service stopped.");
+                    _logger.LogInformation($"{serviceName} service stopped." );
                 }
             }
         }
